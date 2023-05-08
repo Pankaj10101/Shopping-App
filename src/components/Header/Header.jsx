564d6c5035457803f881b9bd55e5c11b685c06f8
Pankaj10101 <rkay1048@gmail.com>
2023-05-08 18:44:07 +0530

    footer added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.scss'
 import {TbSearch} from 'react-icons/tb';
 import {CgShoppingCart} from 'react-icons/cg'
@@ -10,8 +10,22 @@ import Cart from '../Cart/Cart'
 import { Context } from '../../store/context';
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false)
+  const handelScroll= ()=>{
+    const offset = window.scrollY;
+    if(offset>200){
+      setScrolled(true)
+    }else{
+      setScrolled(false)
+    }
+  }
+
+  useEffect(()=>{
+    window.addEventListener("scroll", handelScroll)
+  },[])
+
   return (
-    <header className='main-header' >
+    <header className={`main-header ${scrolled? 'sticky-header':""}`}  >
     <div className='header-content'>
     <ul className='left' >
       <li>Home</li>
